af4bec1f2b5c57d373737097947a6a73ae9dabf8
saigusay <93856619+saigusay@users.noreply.github.com>
2023-05-30 14:35:15 +0900

    Update functions.R

--- code/functions.R
+++ code/functions.R
@@ -1,59 +1,86 @@
 
-xiest <- function(eta=1,tau,y,x,be=matrix(0,nrow=ncol(x),ncol=1)){
-	np = sum(y); ne = nrow(y)
-	be.pre=be; p = length(be)
-	difmax=Inf; difth = 10^-2; it2=0; le.pre = Inf; ll = dd = NULL; ka0=NaN
+logit=function(x)log(x/(1-x))
+
+expit=function(x)1/(1+exp(-x))
+
+xiest <- function(eta=1,tau,y,x,z,be=matrix(0,nrow=ncol(x),ncol=1),al=matrix(0,nrow=ncol(z),ncol=1)){
+	ne = nrow(y)
+	be.pre=be; dimbe = length(be)
+	al.pre=al; dimal = length(al)
+	fp = function(t)1-(1+t)^-1
+	difmax=Inf; difth = 10^-2; it2=0; le.pre = Inf; ll = dd = Inf; ka0=NaN
 	while((!is.nan(difmax) & difmax>difth) & it2<iter.cal){
 		it2 = it2+1; upd = F
-		g1 = g1n = g2n = h2n = matrix(0,p,1); g1d = g2d = 0
-		h1 = h1n = h2n = matrix(0,p,p)
+		gb = matrix(0,dimbe,1); hbb = matrix(0,dimbe,dimbe)
+		ga = matrix(0,dimal,1); haa = matrix(0,dimal,dimal); hba = matrix(0,dimbe,dimal)
 		for(id in 1:ne){
 			yi = y[id]; wi = 1/max(c(1,yi))
-			lp = x[id,]%*%be; lam = exp(lp)
-			for(i in 1:max(c(1,yi))){
-				if(tau==Inf){
-					g1 = g1 + ((yi>0) - wi*lam)%x%matrix(x[id,],ncol=1)
-					h1 = h1 - (-wi*lam)%x%(t(x[id,])%x%x[id,])
-				}else{
-					g1 = g1 + c(1-(1+eta*tau*lam)^(-1/eta))*((yi>0) - wi*lam)%x%matrix(x[id,],ncol=1)
-					h1 = h1 - (-eta*tau*lam*(1+eta*tau*lam)^(-(1+eta)/eta)*((yi>0) - wi*lam) + (1-(1+eta*tau*lam)^(-1/eta))*(-wi*lam))%x%(t(x[id,])%x%x[id,])
-				}
+			bw = x[id,]%*%be; lam = c(exp(bw))
+			az = z[id,]%*%al; dp = c(1/(1+exp(-az))); pp = c(1/(1+exp(az)))
+			if(tau==Inf){
+				gb = gb + max(c(1,yi)) * ((yi>0) - wi*lam*dp)%x%matrix(x[id,],ncol=1)
+				ga = ga + max(c(1,yi)) * pp*((yi>0) - wi*lam*dp)%x%matrix(z[id,],ncol=1)
+				hbb = hbb - - max(c(1,yi)) * (wi*lam*dp)%x%(t(x[id,])%x%x[id,])
+				hba = hba - - max(c(1,yi)) * (pp*wi*lam*dp)%x%((x[id,])%x%t(z[id,]))
+				haa = haa - - max(c(1,yi)) * (dp*pp*(wi*lam*pp + (yi>0)-wi*lam*dp))%x%(t(z[id,])%x%z[id,])
+			}else{
+				gb = gb + max(c(1,yi)) * (fp(tau*lam*dp))*((yi>0) - wi*lam*dp)%x%matrix(x[id,],ncol=1)
+				ga = ga + max(c(1,yi)) * (fp(tau*lam*dp))*pp*((yi>0) - wi*lam*dp)%x%matrix(z[id,],ncol=1)
+				hbb = hbb - max(c(1,yi)) * (fp(tau*lam*dp)*(((yi>0)-wi*lam*dp)/(1+tau*lam*dp) - wi*lam*dp))%x%(t(x[id,])%x%x[id,])
+				hba = hba - max(c(1,yi)) * (fp(tau*lam*dp)*pp*(((yi>0)-wi*lam*dp)/(1+tau*lam*dp)-wi*lam*dp))%x%((x[id,])%x%t(z[id,]))
+				haa = haa - max(c(1,yi)) * (fp(tau*lam*dp)*pp*(-dp*((yi>0)-wi*lam*dp) + pp/(1+tau*lam*dp)*((yi>0)-wi*lam*dp) - pp*wi*lam*dp))%x%(t(z[id,])%x%z[id,])
 			}
 		}
-		A=try(ginv(h1),silent=T)
-		if(class(A)[1]!="try-error"){
-			be = be + A%*%g1 * mit; upd = T
+		G = matrix(c(gb,ga),ncol=1)
+		H = as.matrix(rbind(cbind(hbb,hba),cbind(t(hba),haa)))
+		Hinv = try(ginv(H),silent=T)
+		if(class(Hinv)[1]!="try-error"){
+			HG = Hinv%*%G
+			be = be + HG[1:dimbe,] * mit
+			al = al + HG[(dimbe+1):(dimbe+dimal),] * mit
+			upd = T
 		}else{
 			upd = F
 		}
-		le = NA
+		le = 0
+		for(id in 1:ne){
+			yi = y[id]; wi = 1/max(c(1,yi))
+			bw = x[id,]%*%be; lam = c(exp(bw))
+			az = z[id,]%*%al; dp = c(1/(1+exp(-az))); pp = c(1/(1+exp(az)))
+			if(tau==Inf){
+				le = le - max(c(1,yi))*((yi>0)*log(lam*dp)-wi*lam*dp)
+			}else{
+				le = le - max(c(1,yi))*(((yi>0)+wi/tau)*log(1+tau*lam*dp)-wi*lam*dp)
+			}
+		}
 		ll = c(ll,le)
-		difmax = max(abs(c(be-be.pre)))
+		difmax = abs(ll[length(ll)-1]-le)
+		#difmax = max(abs(c(be-be.pre,al-al.pre)))
 		dd = c(dd,difmax)
-		be.pre=be
+		be.pre=be; al.pre=al
 	} #while
 	conv = upd & difmax<difth
 	if(is.na(conv))conv=F
 
-	#return(list(be=be,b=b,th=th,lgg=lgg,conv=conv,difmax=difmax,cll=cll,Lgg=Lgg,rhh=rhh))
-	return(list(be=be,conv=conv,difmax=difmax,it2=it2))
+	return(list(be=be,al=al,conv=conv,difmax=difmax,it2=it2))
 }
 
-xipest <- function(eta=1,tau,y,x,be0=matrix(0,nrow=ncol(x),ncol=1),phi){
-	be0[abs(be0)<10^-10] = 0
+xitpest <- function(eta=1,tau,y,x,z,be=matrix(0,nrow=ncol(x),ncol=1),phi,al=matrix(0,nrow=ncol(z),ncol=1)){
+	be[abs(be)<10^-10] = 0
 	np = sum(y); ne = nrow(y)
-	be.pre = be = be0; p = length(be0)
+	be.pre = be; dimbe = length(be)
+	al.pre = al; dimal = length(al)
+	fp = function(t)1-(1+t)^-1
 	lxp = function(rho){
-		l = 0; beopt = be+rho*gp
+		l = 0; beopt = be + rho*gp
 		for(id in 1:ne){
 			yi = y[id]; wi = 1/max(c(1,yi))
-			lp = x[id,]%*%beopt; lam = exp(lp)
-			for(i in 1:max(c(1,yi))){
-				if(tau==Inf){
-					l = l - (yi>0)*log(lam) + wi*lam
-				}else{
-					l = l - ((yi>0)+wi/tau)*log(1+tau*lam) + wi*lam
-				}
+			bx = x[id,]%*%beopt; lam = exp(bx)
+			az = z[id,]%*%al; dp = c(1/(1+exp(-az)))
+			if(tau==Inf){
+				l = l - max(c(1,yi)) * ((yi>0)*log(lam*dp) - wi*lam*dp)
+			}else{
+				l = l - max(c(1,yi)) * (((yi>0)+wi/tau)*log(1+tau*lam*dp) - wi*lam*dp)
 			}
 		}
 		l = l + phi*sum(abs(beopt[-1]))
@@ -61,49 +88,74 @@ xipest <- function(eta=1,tau,y,x,be0=matrix(0,nrow=ncol(x),ncol=1),phi){
 	}
 	difmax=-Inf; difth = 10^-2; it2=0; le.pre = Inf; dd = NULL; llp = Inf; ka0=NaN
 	while((!is.nan(difmax) & -difmax>difth) & it2<iter.cal){
-		it2 = it2+1
-		g1 = g1n = g2n = h2n = matrix(0,p,1); g1d = g2d = 0
-		h1 = h1n = h2n = matrix(0,p,p)
+		it2 = it2+1; upd = T
+
+		ga = matrix(0,dimal,1); haa = matrix(0,dimal,dimal)
 		for(id in 1:ne){
 			yi = y[id]; wi = 1/max(c(1,yi))
-			lp = x[id,]%*%be; lam = exp(lp)
-			for(i in 1:max(c(1,yi))){
-				if(tau==Inf){
-					g1 = g1 + ((yi>0) - wi*lam)%x%matrix(x[id,],ncol=1)
-					#h1 = h1 - (-wi*lam)%x%(t(x[id,])%x%x[id,])
-				}else{
-					g1 = g1 + c(1-(1+eta*tau*lam)^(-1/eta))*((yi>0) - wi*lam)%x%matrix(x[id,],ncol=1)
-					#h1 = h1 - (-eta*tau*lam*(1+eta*tau*lam)^(-(1+eta)/eta)*((yi>0) - wi*lam) + (1-(1+eta*tau*lam)^(-1/eta))*(-wi*lam))%x%(t(x[id,])%x%x[id,])
-				}
+			bx = x[id,]%*%be; lam = c(exp(bx))
+			az = z[id,]%*%al; dp = c(1/(1+exp(-az))); pp = c(1/(1+exp(az)))
+			if(tau==Inf){
+				ga = ga + max(c(1,yi)) * pp*((yi>0) - wi*lam*dp)%x%matrix(z[id,],ncol=1)
+				haa = haa - - max(c(1,yi)) * (dp*pp*(wi*lam*pp + (yi>0)-wi*lam*dp))%x%(t(z[id,])%x%z[id,])
+			}else{
+				ga = ga + max(c(1,yi)) * fp(tau*lam*dp)*pp*((yi>0) - wi*lam*dp)%x%matrix(z[id,],ncol=1)
+				haa = haa - max(c(1,yi)) * (fp(tau*lam*dp)*pp*(-dp*((yi>0)-wi*lam*dp) + pp/(1+tau*lam*dp)*((yi>0)-wi*lam*dp) - pp*wi*lam*dp))%x%(t(z[id,])%x%z[id,])
 			}
 		}
+		A = try(ginv(haa),silent=T)
+		if(class(A)[1]!="try-error"){
+			al <- al + A%*%ga
+		}
+		gb = matrix(0,dimbe,1)
+		for(id in 1:ne){
+			yi = y[id]; wi = 1/max(c(1,yi))
+			bx = x[id,]%*%be; lam = c(exp(bx))
+			az = z[id,]%*%al; dp = c(1/(1+exp(-az)))
+			if(tau==Inf){
+				gb = gb + max(c(1,yi)) * ((yi>0) - wi*lam*dp)%x%matrix(x[id,],ncol=1)
+			}else{
+				gb = gb + max(c(1,yi)) * fp(tau*lam*dp)*((yi>0) - wi*lam*dp)%x%matrix(x[id,],ncol=1)
+			}
+		}
+		if(sum(is.nan(gb))>0){ gb<-rep(0,length(be)); upd = F}
 		gp <- NULL
-		for(i in 1:p){
+		for(i in 1:dimbe){
 			if(i==1){
-				gp <- c(gp,g1[i])
+				gp <- c(gp,gb[i])
 			}else if(be[i]!=0){
-				gp <- c(gp, g1[i]-phi*sign(be[i]))
-			}else if(abs(g1[i])>phi){
-				gp <- c(gp, g1[i]-phi*sign(g1[i]))
+				gp <- c(gp, gb[i]-phi*sign(be[i]))
+			}else if(abs(gb[i])>phi){
+				gp <- c(gp, gb[i]-phi*sign(gb[i]))
 			}else{
 				gp <- c(gp, 0)
 			}
 		}
 		A = sign(be)==-sign(gp) & be!=0; A[1] = F
-		rh = be/gp; rh[abs(rh) < 10^-10] = 0
-		rhedg = ifelse(sum(A)>0,min(-rh[A]),10^4)
-		A = try(optim(0,lxp,lower=0,upper=rhedg,method="L"))
+		rh = -be/gp; rh[abs(rh) < 10^-10] = 0
+		rhedg = ifelse(sum(A)>0,min(rh[A]),10^4)
+		A = try(optim(0,lxp,lower=0,upper=rhedg,method="L"),silent=T)
 		rhopt = ifelse(class(A)[1]!="try-error",A$par,0)
-
 		llp = c(llp,lxp(rhopt))
 		difmax = lxp(rhopt) - lxp(0)
-		be <- be + rhopt*gp * mit
-		be[abs(be)<10^-10] = 0
+		be <- be + rhopt*gp
+		be[abs(be)<10^-10] <- 0
 		dd = c(dd,difmax)
 		be.pre=be
 	} #while
-	conv = -difmax<difth
+	conv = upd & -difmax<difth
 	if(is.na(conv))conv=F
-if(!conv)print(dd[(length(dd)-10):length(dd)])
-	return(list(be=be,conv=conv,difmax=difmax,it2=it2))
+if(!conv)print(dd)
+
+	weight = NULL
+	for(id in 1:ne){
+		bw = x[id,]%*%be; lam = c(exp(bw))
+		az = z[id,]%*%al; dp = c(1/(1+exp(-az)))
+		if(tau==Inf){
+			weight = c(weight,1)
+		}else{
+			weight = c(weight,fp(tau*lam*dp))
+		}
+	}
+	return(list(be=be,al=al,weight=weight,conv=conv,difmax=difmax,it2=it2))
 }
